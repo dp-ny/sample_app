4b72a76c01abc4f4163db136ca597093f8d6347e
Daniel Pomeroy <dcpomeroy@mac.com>
2012-05-16 12:12:30 -0400

    Add exercise commands, including user example

---
+++ example_user.rb
@@ -0,0 +1,13 @@
+# example_user.rb
+class User
+	attr_accessor :name, :email
+
+	def initialize(attributes = {})
+		@name = attributes[:name]
+		@email = attributes[:email]
+	end
+
+	def formatted_email
+		"#{@name} <#{@email}>"
+	end
+end
