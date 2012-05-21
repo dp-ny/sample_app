e7da6f92bb91d3134b0ca788a68cebd7dffe067f
Daniel Pomeroy <dcpomeroy@mac.com>
2012-05-21 18:58:42 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,7 @@ describe User do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:password_digest) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 
 
@@ -107,6 +108,11 @@ describe User do
 		end
 
 		it { should_not be_valid }
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 
 end
